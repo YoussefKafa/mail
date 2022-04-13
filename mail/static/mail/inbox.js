0e95ccc10b6fe6ac1c1119f5879a24843e791642
youssefkafa <youssef.kafa19941@gmail.com>
2022-04-12 23:53:08 -0700

    send mail

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -1,33 +1,50 @@
 document.addEventListener('DOMContentLoaded', function() {
-
-  // Use buttons to toggle between views
   document.querySelector('#inbox').addEventListener('click', () => load_mailbox('inbox'));
   document.querySelector('#sent').addEventListener('click', () => load_mailbox('sent'));
   document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
   document.querySelector('#compose').addEventListener('click', compose_email);
-
-  // By default, load the inbox
+  document.querySelector('#compose-form').onsubmit = send_mail;
   load_mailbox('inbox');
 });
-
 function compose_email() {
-
-  // Show compose view and hide other views
   document.querySelector('#emails-view').style.display = 'none';
   document.querySelector('#compose-view').style.display = 'block';
-
-  // Clear out composition fields
+  document.querySelector('#mail-view').style.display = 'none';
   document.querySelector('#compose-recipients').value = '';
   document.querySelector('#compose-subject').value = '';
   document.querySelector('#compose-body').value = '';
 }
-
+function send_mail(){
+  fetch('/emails', {
+      method: 'POST',
+      body: JSON.stringify({
+          recipients: document.querySelector('#compose-recipients').value,
+          subject: document.querySelector('#compose-subject').value,
+          body: document.querySelector('#compose-body').value
+      })
+  }).then(response => response.json()).then(res => { load_mailbox('sent');});
+  return false;
+}
 function load_mailbox(mailbox) {
-  
-  // Show the mailbox and hide other views
-  document.querySelector('#emails-view').style.display = 'block';
-  document.querySelector('#compose-view').style.display = 'none';
-
-  // Show the mailbox name
-  document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+  fetch(`/emails/${mailbox}`)
+  .then(response => response.json())
+  .then(emails => {
+      for (let i of Object.keys(emails)) {
+          const email = document.createElement('div');
+          email.innerHTML = `
+              <div><b>Subject:</b> ${emails[i].subject}</div>
+              <div><b>From:</b> ${emails[i].sender}</div>
+              <div><b>Date:</b> ${emails[i].timestamp}</div>
+          `;
+          if (emails[i].read) {
+            email.classList.add('seen');
+        }
+        email.classList.add('email');
+          document.querySelector('#emails-view').append(email);
+      };
+  });
+document.querySelector('#emails-view').style.display = 'block';
+document.querySelector('#compose-view').style.display = 'none';
+document.querySelector('#mail-view').style.display = 'none';
+document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
 }
